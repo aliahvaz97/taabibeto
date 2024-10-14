21ceb83c682586339f95e39b27fcbf5a084d42ea
a.roshandelpoor <a.roshandelpour@outlook.com>
2024-10-14 18:42:52 +0330

    fix server folder

--- MyNewProject/App.js
+++ MyNewProject/App.js
@@ -1,12 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import MedicalServices from './body/MedicalServices'; // اضافه کردن کامپوننت MedicalServices
-const usersRoute = require('../server/routes/user-route')
-const express = require('express')
-require ('dotenv').config()
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+
 
 export default function App() {
   return (
@@ -23,11 +18,4 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
   },
-});
-
-app.use('/api/users', usersRoute.router);
-
-const port = process.env.APP_PORT || 3000;
-app.listen (port,()=>{
-    console.log(`Listening port ${port}`)
 });
---
+++ server/app.js
@@ -0,0 +1,13 @@
+const usersRoute = require('../server/routes/user-route')
+const express = require('express')
+require ('dotenv').config()
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api/users', usersRoute.router);
+
+const port = process.env.APP_PORT || 3000;
+app.listen (port,()=>{
+    console.log(`Listening port ${port}`)
+});
