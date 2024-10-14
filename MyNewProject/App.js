6f2c2a10772605e655f32cc2b1e0a69d3b24ee13
a.roshandelpoor <a.roshandelpour@outlook.com>
2024-10-14 16:27:58 +0330

    import many files on server folder

--- MyNewProject/App.js
+++ MyNewProject/App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import MedicalServices from './body/MedicalServices'; // اضافه کردن کامپوننت MedicalServices
+const usersRoute = require('../server/routes/user-route')
+const express = require('express')
+require ('dotenv').config()
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 export default function App() {
   return (
@@ -17,4 +23,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
   },
+});
+
+app.use('/api/users', usersRoute.router);
+
+const port = process.env.APP_PORT || 3000;
+app.listen (port,()=>{
+    console.log(`Listening port ${port}`)
 });
